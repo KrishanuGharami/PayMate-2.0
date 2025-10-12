b5d9591c288dfcd950ff0041dab00da5c40329fb
Krishanu Gharami <24krishanugharami@gmail.com>
2025-10-12 23:03:37 +0000

    I have github repository: https://github.com/KrishanuGharami/PayMate/
    B

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -24,6 +25,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+  }
 };
 
 export default nextConfig;
